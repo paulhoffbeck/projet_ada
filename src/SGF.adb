faece8b697e0984fa9d6f4dc2a94b5f9effa667f
paulhoffbeck <hoffbeck.paul@gmail.com>
2026-01-12 14:27:10 +0100

    ajout méthode init_SGF dans SGF.adb

--- src/SGF.adb
+++ src/SGF.adb
@@ -1,4 +1,10 @@
 with Ada.Text_IO; use Ada.Text_IO;
 package body SGF is 
-
+procedure Init_SGF (Dos : out T_Dossier) is
+begin
+   Dos.Nom := "/";
+   Dos.Droits := 2#111#; -- read, write, exec
+   Dos.Dossier_Parent := null;
+   Dos.Contenu := null;
+end Init_SGF;
 end SGF;
