1f3e171b2a91cc7ccbd90e3655911d775fc8f463
paulhoffbeck <hoffbeck.paul@gmail.com>
2026-01-16 09:10:17 +0100

    Cd fonctionnel + Mkdir fonctionnel + Main obsolette à changer)

--- src/main.adb
+++ src/main.adb
@@ -1,16 +1,40 @@
-with SGF;
-with Ada.Text_IO; use Ada.Text_IO;
+with SGF; use SGF;
+with Ada.Text_IO;         use Ada.Text_IO;
 with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
 
 procedure Main is
    Fi : SGF.T_Fichier;
-   nom_fichier : String := "Bonjour.txt";
-   Liste : SGF.Liste_U_String := SGF.Split("../local/bin");
+   nom_fichier : constant String := "Bonjour.txt";
+
+   -- Petite procédure locale pour afficher l’arborescence
+   procedure Affiche_Arbre (Dos : SGF.P_Dossier; Indent : Integer := 0) is
+      Elem : SGF.P_Liste_Contenu;
+   begin
+      for I in 1 .. Indent loop
+         Put("  ");
+      end loop;
+      Put_Line(To_String(Dos.all.Nom) & "/");
+
+      Elem := Dos.all.Contenu;
+      while Elem /= null loop
+         if Elem.all.Est_Fichier then
+            for I in 1 .. Indent + 1 loop
+               Put("  ");
+            end loop;
+            Put_Line("- " & To_String(Elem.all.Fichier.all.Nom));
+         else
+            Affiche_Arbre(Elem.all.Dossier, Indent + 1);
+         end if;
+         Elem := Elem.all.Suivant;
+      end loop;
+   end Affiche_Arbre;
+
 begin
+   -- Initialisation
    SGF.Init_SGF;
+   Put_Line("Répertoire courant après Init : " & To_String(SGF.Racine.Nom));
 
-   Put_Line(To_String(SGF.Racine.Nom));
-
+   -- Création d’un fichier à la racine
    SGF.Touch(Fi => Fi, Nom => nom_fichier, Droits => 2#110#);
    Put_Line(To_String(SGF.Racine.Contenu.all.Fichier.all.Nom));
 
